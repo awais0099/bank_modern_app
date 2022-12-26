ead1ad60f88e4e6a5233a7fee8fccd360e97739a
Tester <helotesting09@gmail.com>
2022-12-26 18:27:56 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+    	<div className="sm:px-16 px-6 "></div>
+    </div>
+  )
+}
+
+export default App
